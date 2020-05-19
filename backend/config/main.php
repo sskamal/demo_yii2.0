acfc2e15d6c3bd149a44dcd70b72f19dc8fc1f43
sskamal <kamal1984dev@gmail.com>
2020-05-19 11:36:54 +0000

    Installation template ADMIN LTE 2.0

--- backend/config/main.php
+++ backend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-backend',
+    'name'=>'Yii-demo 2.0',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'YII-DEMO  2.0';
 ?>
 <div class="site-index">
 
